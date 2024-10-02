cee68435c9a67fcd86927117084a554b61a13e41
oren <108007797+tobeoren@users.noreply.github.com>
2024-10-02 11:41:21 +0700

    Update tts_EN.py

--- tts_EN.py
+++ tts_EN.py
@@ -8,12 +8,16 @@ import os
 from rich.console import Console
 from rich.text import Text
 
+# Inisialisasi console rich
+console = Console()
+
 # Menampilkan watermark di bagian awal
 watermark_text = Text("Text to Speach - YouTube Live Chat", style="bold blue")
 author_text = Text("by: tobeoren", style="bold cyan")
 
 console.print(watermark_text, justify="center")
 console.print(author_text, justify="center")
+
 # Daftar pengguna yang ingin diabaikan dalam chat (misalnya nama streamer)
 blacklist = []
 
